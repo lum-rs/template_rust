c206fb4ff63fe6da72daf7f5e8bfff7e0c1e06bb
Torben Schweren <torben@schweren.dev>
2024-11-28 18:06:26 +0100

    fix: rust-analyzer extension crashin when using nix-env-selector

--- shell.nix
+++ shell.nix
@@ -3,17 +3,24 @@
 }:
 
 pkgs.mkShell {
+
   buildInputs = [
     pkgs.rustup
   ];
 
+  TEMPDIR = "/tmp/rust/shell";
+  TMPDIR = "/tmp/rust/shell";
+  TEMP = "/tmp/rust/shell";
+  TMP = "/tmp/rust/shell";
+
   shellHook = ''
-    DATA_DIR=/tmp/rust
-    export RUSTUP_HOME=$DATA_DIR/rustup
-    export CARGO_HOME=$DATA_DIR/cargo
-    export PATH=$CARGO_HOME/bin:$PATH
-    mkdir -p $CARGO_HOME
-    mkdir -p $RUSTUP_HOME
+    DATA_DIR="/tmp/rust"
+    export RUSTUP_HOME="$DATA_DIR/rustup"
+    export CARGO_HOME="$DATA_DIR/cargo"
+    export PATH=$CARGO_niHOME/bin:$PATH
+    mkdir -p "$CARGO_HOME"
+    mkdir -p "$RUSTUP_HOME"
+    mkdir -p "$DATA_DIR/shell"
 
     rustup default stable
     rustup update
@@ -25,6 +32,8 @@ pkgs.mkShell {
 
     echo "Rustup installed at $RUSTUP_HOME"
     echo "Cargo installed at $CARGO_HOME"
-    echo $(cargo --version)
+    echo "$(rustup --version)"
+    echo "$(cargo --version)"
   '';
+
 }
