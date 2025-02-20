b5058b3b44d0097cf682128c27acbefe4828419d
Torben Schweren <torben@schweren.dev>
2025-02-20 19:34:50 +0100

    fix: typo in shell.nix

--- shell.nix
+++ shell.nix
@@ -3,7 +3,6 @@
 }:
 
 pkgs.mkShell {
-
   buildInputs = [
     pkgs.rustup
   ];
@@ -17,7 +16,7 @@ pkgs.mkShell {
     DATA_DIR="/tmp/rust"
     export RUSTUP_HOME="$DATA_DIR/rustup"
     export CARGO_HOME="$DATA_DIR/cargo"
-    export PATH=$CARGO_niHOME/bin:$PATH
+    export PATH=$CARGO_HOME/bin:$PATH
     mkdir -p "$CARGO_HOME"
     mkdir -p "$RUSTUP_HOME"
     mkdir -p "$DATA_DIR/shell"
@@ -35,5 +34,4 @@ pkgs.mkShell {
     echo "$(rustup --version)"
     echo "$(cargo --version)"
   '';
-
 }
